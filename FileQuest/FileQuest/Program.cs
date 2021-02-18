7aae54b91751aa86a92846347972568746b8b417
DominikSiman <74675192+DominikSiman@users.noreply.github.com>
2021-02-18 20:20:50 +0100

    xx
    xx

---
+++ FileQuest/FileQuest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileQuest
+{
+    class Program
+    {
+        static void List1(string miesto, string najdito)
+        {
+            try
+            {
+                IEnumerable<string> fileNames = Directory.GetFiles(miesto, najdito);
+                IEnumerable<FileInfo> fileInfos = fileNames.Select(f => new FileInfo(f));
+                foreach (FileInfo fileInfo in fileInfos)
+                {
+                    Console.WriteLine($@"{fileInfo.Name}({fileInfo.LastWriteTime})");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static IEnumerable<string> GetFiles(string path, string srcPatters)
+        {
+            var files = Directory.EnumerateFiles(path, srcPatters);
+            return files;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("zadajte adresu cieloveho suboru");
+            var path = Path.Combine(Console.ReadLine());
+            List1(path, "*");
+            Console.ReadLine();
+        }
+    }
+}
